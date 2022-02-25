03c43fbb2c57497a1c99f890774dea007745c9d8
Jackson <dinofreud@gmail.com>
2022-02-25 15:39:08 -0800

    add math and char window,confirms

--- script.js
+++ script.js
@@ -8,13 +8,33 @@ var generateBtn = document.querySelector("#generate");
     ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
   ];
-
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+
+    function generatePassword(){
+      var passwordType = [];                             
+     
+      getPasswordType(chars, passwordType, newPassword);
+      var newPassword = "";
+      if(passwordType.length !== 0){
+        var passwordLength = getPasswordLength();
+        for(var i = 0; i < passwordLength; i++){
+          newPassword += passwordType[Math.floor(Math.random() * passwordType.length)];
+        }
+      }
+      return newPassword; 
+    }
+  } 
+    function getPasswordType(allChar, userChar, passNew){
+       //prompts the user for char selection 
+      var windowAnswer1 = window.confirm("Uppercase?");
+      var windowAnswer2 = window.confirm("Lowercase?");
+      var windowAnswer3 = window.confirm("Numbers?");
+      var windowAnswer4 = window.confirm("Special characters?");
 
 }
 
